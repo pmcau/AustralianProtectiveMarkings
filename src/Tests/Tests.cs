13dbb0468a6a8bac9f2557af6a515b7d88736aec
Simon <simon.cropp@gmail.com>
2023-03-28 09:51:18 +1100

    Update Tests.cs

--- src/Tests/Tests.cs
+++ src/Tests/Tests.cs
@@ -1,8 +1,15 @@
+using Argon;
+using AustralianProtectiveMarkings;
+
 [TestFixture]
 public class Tests
 {
     [Test]
     public void Serialization()
     {
+        var protectiveMarking = new ProtectiveMarking(SecurityClassification.Secret);
+        var serialized = JsonConvert.SerializeObject(protectiveMarking);
+        var deserialized = JsonConvert.DeserializeObject<ProtectiveMarking>(serialized);
+        Assert.AreEqual(protectiveMarking, deserialized);
     }
 }
