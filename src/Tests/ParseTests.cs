eb916d7bc78d04031242c4e9764677d91cd22020
Simon <simon.cropp@gmail.com>
2023-03-28 09:14:35 +1100

    .

--- src/Tests/ParseTests.cs
+++ src/Tests/ParseTests.cs
@@ -4,17 +4,270 @@ public class ParseTests
     [Test]
     public void GetAustralia()
     {
-        var data2016 = DataLoader.Maps2016.GetAustralia();
-        Assert.NotEmpty(data2016);
-        Assert.NotNull(data2016);
-        var data2019 = DataLoader.Maps2019.GetAustralia();
-        Assert.NotEmpty(data2019);
-        Assert.NotNull(data2019);
-        var data2022 = DataLoader.Maps2022.GetAustralia();
-        Assert.NotEmpty(data2022);
-        Assert.NotNull(data2022);
-        // var dataFuture = DataLoader.MapsFuture.GetAustralia();
-        // Assert.NotEmpty(dataFuture);
-        // Assert.NotNull(dataFuture);
+        var x = @"ABW	Aruba
+AFG	Afghanistan
+AGO	Angola
+AIA	Anguilla
+ALA	Åland Islands
+ALB	Albania
+AND	Andorra
+ARE	United Arab Emirates
+ARG	Argentina
+ARM	Armenia
+ASM	American Samoa
+ATA	Antarctica
+ATF	French Southern Territories
+ATG	Antigua and Barbuda
+AUS	Australia
+AUT	Austria
+AZE	Azerbaijan
+BDI	Burundi
+BEL	Belgium
+BEN	Benin
+BES	Caribbean Netherlands
+BFA	Burkina Faso
+BGD	Bangladesh
+BGR	Bulgaria
+BHR	Bahrain
+BHS	Bahamas
+BIH	Bosnia and Herzegovina
+BLM	Saint Barthélemy
+BLR	Belarus
+BLZ	Belize
+BMU	Bermuda
+BOL	Bolivia
+BRA	Brazil
+BRB	Barbados
+BRN	Brunei
+BTN	Bhutan
+BVT	Bouvet Island
+BWA	Botswana
+CAF	Central African Republic
+CAN	Canada
+CCK	CocosKeelingIslands
+CHE	Switzerland
+CHL	Chile
+CHN	China
+CIV	Ivory Coast
+CMR	Cameroon
+COD	Democratic Republic of the Congo
+COG	Republic of the Congo
+COK	Cook Islands
+COL	Colombia
+COM	Comoros
+CPV	Cabo Verde
+CRI	Costa Rica
+CUB	Cuba
+CUW	Curaçao
+CXR	Christmas Island
+CYM	Cayman Islands
+CYP	Cyprus
+CZE	Czechia
+DEU	Germany
+DJI	Djibouti
+DMA	Dominica
+DNK	Denmark
+DOM	Dominican Republic
+DZA	Algeria
+ECU	Ecuador
+EGY	Egypt
+ERI	Eritrea
+ESH	Western Sahara
+ESP	Spain
+EST	Estonia
+ETH	Ethiopia
+FIN	Finland
+FJI	Fiji
+FLK	Falkland Islands
+FRA	France
+FRO	Faroe Islands
+FSM	Micronesia
+GAB	Gabon
+GBR	United Kingdom
+GEO	Georgia
+GGY	Guernsey
+GHA	Ghana
+GIB	Gibraltar
+GIN	Guinea
+GLP	Guadeloupe
+GMB	Gambia
+GNB	Guinea-Bissau
+GNQ	Equatorial Guinea
+GRC	Greece
+GRD	Grenada
+GRL	Greenland
+GTM	Guatemala
+GUF	French Guiana
+GUM	Guam
+GUY	Guyana
+HKG	Hong Kong
+HMD	Heard Island and McDonald Islands
+HND	Honduras
+HRV	Croatia
+HTI	Haiti
+HUN	Hungary
+IDN	Indonesia
+IMN	Isle of Man
+IND	India
+IOT	British Indian Ocean Territory
+IRL	Ireland
+IRN	Iran
+IRQ	Iraq
+ISL	Iceland
+ISR	Israel
+ITA	Italy
+JAM	Jamaica
+JEY	Jersey
+JOR	Jordan
+JPN	Japan
+KAZ	Kazakhstan
+KEN	Kenya
+KGZ	Kyrgyzstan
+KHM	Cambodia
+KIR	Kiribati
+KNA	Saint Kitts and Nevis
+KOR	South Korea
+KWT	Kuwait
+LAO	Laos
+LBN	Lebanon
+LBR	Liberia
+LBY	Libya
+LCA	Saint Lucia
+LIE	Liechtenstein
+LKA	Sri Lanka
+LSO	Lesotho
+LTU	Lithuania
+LUX	Luxembourg
+LVA	Latvia
+MAC	Macao
+MAF	Collectivity of Saint Martin
+MAR	Morocco
+MCO	Monaco
+MDA	Moldova
+MDG	Madagascar
+MDV	Maldives
+MEX	Mexico
+MHL	Marshall Islands
+MKD	North Macedonia
+MLI	Mali
+MLT	Malta
+MMR	Myanmar
+MNE	Montenegro
+MNG	Mongolia
+MNP	Northern Mariana Islands
+MOZ	Mozambique
+MRT	Mauritania
+MSR	Montserrat
+MTQ	Martinique
+MUS	Mauritius
+MWI	Malawi
+MYS	Malaysia
+MYT	Mayotte
+NAM	Namibia
+NCL	New Caledonia
+NER	Niger
+NFK	Norfolk Island
+NGA	Nigeria
+NIC	Nicaragua
+NIU	Niue
+NLD	Netherlands
+NOR	Norway
+NPL	Nepal
+NRU	Nauru
+NZL	New Zealand
+OMN	Oman
+PAK	Pakistan
+PAN	Panama
+PCN	Pitcairn
+PER	Peru
+PHL	Philippines
+PLW	Palau
+PNG	Papua New Guinea
+POL	Poland
+PRI	Puerto Rico
+PRK	North Korea
+PRT	Portugal
+PRY	Paraguay
+PSE	Palestine
+PYF	French Polynesia
+QAT	Qatar
+REU	Réunion
+ROU	Romania
+RUS	Russia
+RWA	Rwanda
+SAU	Saudi Arabia
+SDN	Sudan
+SEN	Senegal
+SGP	Singapore
+SGS	South Georgia and the South Sandwich Islands
+SHN	Saint Helena, Ascension and Tristan da Cunha
+SJM	Svalbard and Jan Mayen
+SLB	Solomon Islands
+SLE	Sierra Leone
+SLV	El Salvador
+SMR	San Marino
+SOM	Somalia
+SPM	Saint Pierre and Miquelon
+SRB	Serbia
+SSD	South Sudan
+STP	São Tomé and Príncipe
+SUR	Suriname
+SVK	Slovakia
+SVN	Slovenia
+SWE	Sweden
+SWZ	Eswatini
+SXM	Sint Maarten
+SYC	Seychelles
+SYR	Syria
+TCA	Turks and Caicos Islands
+TCD	Chad
+TGO	Togo
+THA	Thailand
+TJK	Tajikistan
+TKL	Tokelau
+TKM	Turkmenistan
+TLS	Timor-Leste
+TON	Tonga
+TTO	Trinidad and Tobago
+TUN	Tunisia
+TUR	Turkey
+TUV	Tuvalu
+TWN	Taiwan
+TZA	Tanzania
+UGA	Uganda
+UKR	Ukraine
+UMI	United States Minor Outlying Islands
+URY	Uruguay
+USA	United States of America
+UZB	Uzbekistan
+VAT	Vatican City
+VCT	Saint Vincent and the Grenadines
+VEN	Venezuela
+VGB	British Virgin Islands
+VIR	United States Virgin Islands
+VNM	Viet Nam
+VUT	Vanuatu
+WLF	Wallis and Futuna
+WSM	Samoa
+YEM	Yemen
+ZAF	South Africa
+ZMB	Zambia
+ZWE	Zimbabwe";
+
+        var dictionary = new Dictionary<string, string>();
+        foreach (var line in x.Split("\r\n"))
+        {
+            var strings = line.Split('	');
+            dictionary[strings[0]] = strings[1];
+        }
+        foreach (var (key, value) in dictionary)
+        {
+            Console.Write($@"
+/// <summary>
+/// {value} - {key}
+/// </summary>
+{value.Replace(" and ","And").Replace(" of ","Of").Replace(" ","").Replace("-","")},
+");
+        }
     }
 }
