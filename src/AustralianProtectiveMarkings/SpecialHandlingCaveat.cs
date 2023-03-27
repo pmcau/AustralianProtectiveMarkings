6d05728e29a6fdffc696968d491a0995543b67c1
Simon <simon.cropp@gmail.com>
2023-03-28 10:13:13 +1100

    Update SpecialHandlingCaveat.cs

--- src/AustralianProtectiveMarkings/SpecialHandlingCaveat.cs
+++ src/AustralianProtectiveMarkings/SpecialHandlingCaveat.cs
@@ -4,4 +4,18 @@ public record struct SpecialHandlingCaveat(
     SpecialHandlingCaveatType Type,
     string? Value = null)
 {
+    public SpecialHandlingCaveat DelicateSource() =>
+        new(SpecialHandlingCaveatType.DelicateSource);
+
+    public SpecialHandlingCaveat Orcon() =>
+        new(SpecialHandlingCaveatType.Orcon);
+
+    public SpecialHandlingCaveat ExclusiveFor(string personOrIndicator) =>
+        new(SpecialHandlingCaveatType.ExclusiveFor, personOrIndicator);
+
+    public SpecialHandlingCaveat Cabinet() =>
+        new(SpecialHandlingCaveatType.Cabinet);
+
+    public SpecialHandlingCaveat Rel() =>
+        new(SpecialHandlingCaveatType.NationalCabinet);
 }
