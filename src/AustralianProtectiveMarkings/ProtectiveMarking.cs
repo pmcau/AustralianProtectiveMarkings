e192fc9129f6ca28654a3f246592caa4a5f7ab53
Simon Cropp <simon.cropp@gmail.com>
2023-03-28 20:20:55 +1100

    Update ProtectiveMarking.cs

--- src/AustralianProtectiveMarkings/ProtectiveMarking.cs
+++ src/AustralianProtectiveMarkings/ProtectiveMarking.cs
@@ -3,9 +3,37 @@ namespace AustralianProtectiveMarkings;
 
 public record struct ProtectiveMarking
 {
+    readonly Expiry? expiry;
     public required SecurityClassification SecurityClassification { get; init; }
     public Caveats? Caveats { get; init; }
-    public Expiry? Expiry { get; init; }
+
+    public Expiry? Expiry
+    {
+        readonly get => expiry;
+        init
+        {
+            if (value == null)
+            {
+                return;
+            }
+
+            var @event = value.Value.Event;
+            var genDate = value.Value.GenDate;
+
+            if (genDate == null && @event == null)
+            {
+                throw new("Either GenDate or Event must be defined");
+            }
+
+            if (genDate != null && @event != null)
+            {
+                throw new("GenDate and Event cannot be both defined.");
+            }
+
+            expiry = value;
+        }
+    }
+
     public IReadOnlyCollection<InformationManagementMarker>? InformationManagementMarkers { get; init; }
     public string? Note { get; init; }
     public string? Origin { get; init; }
