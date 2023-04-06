32115118dae934760b61e15fb691c3c1c4a51cf7
Simon Cropp <simon.cropp@gmail.com>
2023-04-06 21:11:05 +1000

    Update OfficeDoc.cs

--- src/AustralianProtectiveMarkings/OfficeDoc.cs
+++ src/AustralianProtectiveMarkings/OfficeDoc.cs
@@ -8,25 +8,33 @@ public static class OfficeDoc
     {
         var header = marking.RenderEmailHeader();
         using var zip = new ZipArchive(stream, ZipArchiveMode.Update, leaveOpen: true);
-        var zipEntry = zip.Entries.SingleOrDefault(_ => _.FullName == customPropsFileName);
-        if (zipEntry == null)
+        HandleCustomProps(zip, header);
+    }
+
+    static void HandleCustomProps(ZipArchive zip, string header)
+    {
+        var entry = zip.Entries.SingleOrDefault(_ => _.FullName == customPropsFileName);
+        if (entry == null)
         {
-            zipEntry = zip.CreateEntry(customPropsFileName);
-            using var entryStream = zipEntry.Open();
-            using var writer = new StreamWriter(entryStream);
+            entry = zip.CreateEntry(customPropsFileName);
+            using var stream = entry.Open();
+            using var writer = new StreamWriter(stream);
             writer.Write($$"""
-                        <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-                        <Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/custom-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">
-                            <property fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}" pid="2" name="X-Protective-Marking">
-                                <vt:lpwstr>{{header}}</vt:lpwstr>
-                            </property>
-                        </Properties>
-                        """);
+                <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+                <Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/custom-properties"
+                            xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">
+                    <property fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}"
+                              pid="2"
+                              name="X-Protective-Marking">
+                        <vt:lpwstr>{{header}}</vt:lpwstr>
+                    </property>
+                </Properties>
+                """);
         }
         else
         {
-            using var entryStream = zipEntry.Open();
-            using var reader = new StreamReader(entryStream);
+            using var stream = entry.Open();
+            using var reader = new StreamReader(stream);
             var document = new XDocument(reader.ReadToEnd());
             AddHeader(document, header);
         }
@@ -43,10 +51,10 @@ public static class OfficeDoc
         if (property == null)
         {
             var element = XElement.Parse("""
-                    <property fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}"
-                              pid="2"
-                              name="X-Protective-Marking" />
-                    """);
+                <property fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}"
+                          pid="2"
+                          name="X-Protective-Marking" />
+                """);
             element.Add(new XElement(vtNamespace + "lpwstr", marking));
             document.Root!.Add(element);
         }
