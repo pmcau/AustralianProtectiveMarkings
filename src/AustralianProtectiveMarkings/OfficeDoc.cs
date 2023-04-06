e49189844df784a7af7a96e0450e9d8031779ecf
Simon Cropp <simon.cropp@gmail.com>
2023-04-06 20:07:14 +1000

    .

--- src/AustralianProtectiveMarkings/OfficeDoc.cs
+++ src/AustralianProtectiveMarkings/OfficeDoc.cs
@@ -10,14 +10,13 @@ public static class OfficeDoc
     public static void PatchWord(Stream stream, ProtectiveMarking marking)
     {
         var header = marking.RenderEmailHeader();
-        using (var zip = new ZipArchive(stream, ZipArchiveMode.Update, leaveOpen: false))
+        using var zip = new ZipArchive(stream, ZipArchiveMode.Update, leaveOpen: false);
+        var zipArchiveEntry = zip.Entries.SingleOrDefault(_ => _.FullName == customPropsFileName);
+        if (zipArchiveEntry == null)
         {
-            var zipArchiveEntry = zip.Entries.SingleOrDefault(_ => _.FullName == customPropsFileName);
-            if (zipArchiveEntry == null)
-            {
-                zipArchiveEntry = zip.CreateEntry(customPropsFileName);
-                using var writer = new StreamWriter(zipArchiveEntry.Open());
-                writer.Write($$"""
+            zipArchiveEntry = zip.CreateEntry(customPropsFileName);
+            using var writer = new StreamWriter(zipArchiveEntry.Open());
+            writer.Write($$"""
                         <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
                         <Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/custom-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">
                             <property fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}" pid="2" name="X-Protective-Marking">
@@ -25,22 +24,37 @@ public static class OfficeDoc
                             </property>
                         </Properties>
                         """);
-                return;
-            }
-            else
-            {
-                var xDocument = new XDocument(new StreamReader(zipArchiveEntry.Open()).ReadToEnd());
-                var property = xDocument
-                    .Descendants()
-                    .SingleOrDefault(_ => _.Name.LocalName == "property" &&
-                                          _.Attribute("name")?.Value == "X-Protective-Marking");
-                if (property == null)
-                {
-                    xDocument.Root.Add(new XElement("property", "new content"));
-                }
-            }
+            return;
         }
 
-        return Task.CompletedTask;
+        var xDocument = new XDocument(new StreamReader(zipArchiveEntry.Open()).ReadToEnd());
+        AddHeader(xDocument, marking);
+    }
+
+    internal static void AddHeader(XDocument xDocument, ProtectiveMarking marking)
+    {
+        var header = marking.RenderEmailHeader();
+        var property = xDocument
+            .Descendants()
+            .SingleOrDefault(_ => _.Name.LocalName == "property" &&
+                                  _.Attribute("name")?.Value == "X-Protective-Marking");
+        if (property == null)
+        {
+            xDocument.Root!.Add(
+                XElement.Parse($$"""
+                    <property fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}"
+                              pid="2"
+                              name="X-Protective-Marking">
+                        <vt:lpwstr>{{header}}</vt:lpwstr>
+                    </property>
+                    """));
+        }
+        else
+        {
+            var xElement = property
+                .Elements()
+                .Single(_ => _.Name.LocalName == "lpwstr");
+            xElement.Value = header;
+        }
     }
 }
--- src/Tests/OfficeDocTests.cs
+++ src/Tests/OfficeDocTests.cs
@@ -1,11 +1,38 @@
-﻿[TestFixture]
+﻿using System.Xml;
+using System.Xml.Linq;
+
+[TestFixture]
 public class OfficeDocTests
 {
+    // [Test]
+    // public async Task Parse()
+    // {
+    //     var memoryStream = new MemoryStream();
+    //     await Resourcer.Resource.AsStream("doc.docx").CopyToAsync(memoryStream);
+    //     await OfficeDoc.PatchWord(memoryStream);
+    // }
+
     [Test]
-    public async Task Parse()
+    public Task UpdateHeader()
     {
-        var memoryStream = new MemoryStream();
-        await Resourcer.Resource.AsStream("doc.docx").CopyToAsync(memoryStream);
-        await OfficeDoc.PatchWord(memoryStream);
+        var xml = """
+                <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+                <Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/custom-properties"
+                            xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">
+                    <property fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}"
+                              pid="2"
+                              name="X-Protective-Marking">
+                        <vt:lpwstr>oldvalue</vt:lpwstr>
+                    </property>
+                </Properties>
+                """;
+        var document = XDocument.Load(new StringReader(xml));
+        OfficeDoc.AddHeader(
+            document,
+            new()
+            {
+                Classification = Classification.Secret
+            });
+        return Verify(document);
     }
 }
